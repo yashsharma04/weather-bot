b4ebaac754c33dfab6be711382a7c968ca947c5e
Yash Sharma <95yashsharma@gmail.com>
2016-10-18 01:45:41 +0530

    bot speaks

--- index.js
+++ index.js
@@ -45,3 +45,24 @@ app.post('/webhook/', function (req, res) {
 })
 
 var token = "EAAOEcAlTDKABADuCKiLWqcofnTqEnONBe51aN2UTNlbz8r0vMeMQ7sOZBPjPitEJIQcQQMTsvrXhy5alQXikcf5YZBvCrHMZBPtMhRL1il7wF2l5BVROLF2eWqaZAPErElqHKZBKHU4Cx1e7F6d9Ua6zMuZBDFfXZBFbPXElCRsFAZDZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
